97e069ef3ae8a7ffb3c165733dea5fdcf812777a
Augustine <smaji08@gmailcom>
2020-04-19 00:00:29 -0400

    Updates

---
+++ precache-manifest.b510e9b4f1601cefcd8f4e1e6a5c42bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ace3009531a3568cdf5e3fb34c4064f",
+    "url": "/mygarage-kaera/index.html"
+  },
+  {
+    "revision": "1cfc5a461d59f869ce41",
+    "url": "/mygarage-kaera/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "21f7da09d316b9e5c49e",
+    "url": "/mygarage-kaera/static/js/2.364b7792.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mygarage-kaera/static/js/2.364b7792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cfc5a461d59f869ce41",
+    "url": "/mygarage-kaera/static/js/main.268240b0.chunk.js"
+  },
+  {
+    "revision": "79a09e7caeb65d4f9bc6",
+    "url": "/mygarage-kaera/static/js/runtime-main.615734a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mygarage-kaera/static/media/logo.5d5d9eef.svg"
+  }
+]);
