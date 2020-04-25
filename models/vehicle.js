ce0a843ad51fe551d95b4dc099c5509c4ea94576
Augustine <smaji08@gmailcom>
2020-04-24 20:05:04 -0400

    added vehicle Model

---
+++ models/vehicle.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const vehicleSchema = new Schema({
+  vinNumber: {
+    type: String,
+    required: true,
+  },
+
+  make: {
+    type: String,
+    required: true,
+  },
+
+  model: {
+    type: String,
+    required: true,
+  },
+
+  year: {
+    type: Number,
+    required: true,
+  },
+
+  trimLevel: {
+    type: String,
+  },
+
+  engineSize: {
+    type: String,
+  },
+
+  transmission: {
+    type: String,
+  },
+
+  drivetrain: {
+    type: String,
+  },
+
+  presentVal: {
+    type: Number,
+    // required: true,
+  },
+
+  mileage: {
+    type: Number,
+    // required: true,
+  },
+});
+
+const Vehicle = mongoose.model("Vehicle", vehicleSchema);
+
+module.exports = Vehicle;
