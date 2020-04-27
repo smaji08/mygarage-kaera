955ea7ba887803b1bf48dbf917e35266c751c8e8
Augustine <smaji08@gmailcom>
2020-04-27 14:04:09 -0400

    API key in env file and basic routes setup and added some data to seedsDB

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,6 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import MyForm from './garage/VinForm'
-
-// function App() {
-//   return (
-//     <MyForm/>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import MyForm from "./pages/VinForm";
+import RekhaForm from "./pages/VinForm";
 import AugForm from "./pages/Schedule";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
@@ -28,10 +15,13 @@ function App() {
             {/* <EricForm /> */}
           </Route>
           <Route exact path={["/vehicle", "/mygarage-kaera/vehicle"]}>
-            <MyForm />
+            <RekhaForm />
           </Route>
           <Route exact path="/schedule">
             <AugForm />
+          </Route>
+          <Route exact path="/parts">
+            {/* <AndrewForm /> */}
           </Route>
           <Route exact path="/vehicle/:id">
             {/* <Detail /> */}
