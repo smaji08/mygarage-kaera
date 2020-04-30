9f9a02301365a4d7010d00566b92083c86baa8e0
ZVLU <zvluwork@gmail.com>
2020-04-30 19:36:31 -0400

    working on main page

--- models/user.js
+++ models/user.js
@@ -2,17 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: {
+  username: {
     type: String,
     required: true,
   },
-
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-
   password: {
     type: String,
     required: true,
