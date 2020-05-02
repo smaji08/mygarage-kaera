8bf3a86b5b9be78ef61dda2d7aaa39da40a2ee03
AndrewL <lawna12@gmail.com>
2020-05-02 11:04:32 -0400

    movie page

--- client/src/pages/VinForm.js
+++ client/src/pages/VinForm.js
@@ -1,18 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // import ReactDOM from "react-dom";
 import API from "../utils/API";
 
 class MyForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { vinNum: "" };
+    this.state = { 
+      vinNum: "", 
+      car: ""
+    };
   }
   mySubmitHandler = (event) => {
     event.preventDefault();
     alert("You are submitting " + this.state.vinNum);
     API.getCar(this.state.vinNum).then((res) => {
-      console.log(res);
+      this.setState({ car: res })
     });
+
   };
   myChangeHandler = (event) => {
     this.setState({ vinNum: event.target.value });
@@ -20,12 +25,21 @@ class MyForm extends React.Component {
 
   render() {
     return (
+      <>
       <form onSubmit={this.mySubmitHandler}>
         <h1>Hello </h1>
         <p>Please enter your VIN, and submit:</p>
         <input type="text" onChange={this.myChangeHandler} />
         <input type="submit" />
       </form>
+      <Link to={{ 
+        pathname: "/movie",
+        state: this.state.car.data
+        }} 
+        className="movieLink">
+        Movies featuring your car's make and model
+      </Link>
+     </>
     );
   }
 }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,4 +10,13 @@ export default {
         process.env.REACT_APP_API_KEY
     );
   },
-};
+  getMovie: function (vinNum) {
+    return axios.get(
+      "https://marketcheck-prod.apigee.net/v2/decode/car/" +
+        vinNum +
+        // "/specs?api_key=G8wFnLaxeRWBiUvMcHfd3eo7C9KBZQLX"
+        "/specs?api_key=" +
+        process.env.REACT_APP_API_KEY
+    );
+  }
+}
