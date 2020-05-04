d46ccdf1298b91959ff4cf2bc80b0d1bdbf40854
Rekha Ballur <rnmadalli@gmail.com>
2020-05-04 13:38:21 -0400

    vinform and vincard Fixed

---
+++ client/src/components/VinCard/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Card, ListGroup} from "react-bootstrap"
+
+let VinCard = ({ vData }) => {
+  
+  if (vData.make !== undefined || vData.make != null) {
+    return (
+      <div className="card vincard">
+        <Card >
+        <ListGroup variant="flush">
+            <ListGroup.Item><strong>Make:</strong> {vData.make}</ListGroup.Item>
+            <ListGroup.Item><strong>Model:</strong> {vData.model}</ListGroup.Item>
+            <ListGroup.Item><strong>Year:</strong> {vData.year}</ListGroup.Item>
+            <ListGroup.Item><strong>Trim:</strong> {vData.trim}</ListGroup.Item>
+            <ListGroup.Item><strong>Body-Type:</strong> {vData.body_type}</ListGroup.Item>
+            <ListGroup.Item><strong>Transmission:</strong> {vData.transmission}</ListGroup.Item>
+            <ListGroup.Item><strong>Fuel-Type:</strong> {vData.fuel_type}</ListGroup.Item>
+            <ListGroup.Item><strong>Engine:</strong> {vData.engine}</ListGroup.Item>
+            <ListGroup.Item><strong>Drivetrain:</strong> {vData.drivetrain}</ListGroup.Item>
+            <ListGroup.Item><strong>Vehicle-Type:</strong> {vData.vehicle_type}</ListGroup.Item>
+        </ListGroup>
+        </Card>
+      </div>
+    );
+  } else {
+      return <div><span>&nbsp;</span></div>
+  }
+};
+
+export default VinCard;
