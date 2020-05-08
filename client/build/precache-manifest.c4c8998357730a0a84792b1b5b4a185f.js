370084dcc2e95e142d5c15aa001e7d5d9585d0d6
AndrewL <lawna12@gmail.com>
2020-05-07 23:34:43 -0400

    runbuild

---
+++ client/build/precache-manifest.c4c8998357730a0a84792b1b5b4a185f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e506daec53250c8f46ddd28767c87309",
+    "url": "/mygarage-kaera/index.html"
+  },
+  {
+    "revision": "686bada176eede5d1808",
+    "url": "/mygarage-kaera/static/css/2.77035bb8.chunk.css"
+  },
+  {
+    "revision": "c37fa72d35a26a12a38f",
+    "url": "/mygarage-kaera/static/css/main.892ae5be.chunk.css"
+  },
+  {
+    "revision": "686bada176eede5d1808",
+    "url": "/mygarage-kaera/static/js/2.7118ccd6.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/mygarage-kaera/static/js/2.7118ccd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37fa72d35a26a12a38f",
+    "url": "/mygarage-kaera/static/js/main.754de22e.chunk.js"
+  },
+  {
+    "revision": "79a09e7caeb65d4f9bc6",
+    "url": "/mygarage-kaera/static/js/runtime-main.615734a7.js"
+  },
+  {
+    "revision": "6274303b7d9038bcafe75a3caf3d8f18",
+    "url": "/mygarage-kaera/static/media/avatarboy.6274303b.png"
+  },
+  {
+    "revision": "a6a009cc5e01516f99553c2608e67541",
+    "url": "/mygarage-kaera/static/media/mygarage.a6a009cc.jpg"
+  },
+  {
+    "revision": "67c846691580b23dd393a05f06f028ac",
+    "url": "/mygarage-kaera/static/media/parts.67c84669.jpg"
+  },
+  {
+    "revision": "7ca0878db04709e78f607eb5f66f65ea",
+    "url": "/mygarage-kaera/static/media/service.7ca0878d.jpg"
+  },
+  {
+    "revision": "75424f7beb40df30e91b27c9ebee51e0",
+    "url": "/mygarage-kaera/static/media/vinform_bg.75424f7b.jpg"
+  }
+]);
